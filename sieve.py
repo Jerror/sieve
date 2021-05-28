725469b56674422f746bb0bdc1b54823e69e03bf
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2021-05-27 20:55:08 -0400

    use a union dataclass for leaves

--- sieve.py
+++ sieve.py
@@ -1,9 +1,14 @@
 import copy
 import itertools as it
-
 import pandas as pd
-
 from ete3 import Tree
+from dataclasses import dataclass
+from typing import Union
+
+
+@dataclass
+class Leaf:
+    data: Union[pd.Series, str]
 
 
 def gen_sieve(state):
@@ -19,12 +24,14 @@ def gen_sieve(state):
 
 def sieve_stack(state, filters):
     if state is not True:
+        if not isinstance(state, pd.Series):
+            raise RuntimeError
         state = state.copy()
     s = gen_sieve(state)
     next(s)
     keys, masks = zip(*(*filters, (None, None)))
-    return filter(lambda km: isinstance(km[1], pd.Series) and km[1].any(),
-                  zip(keys, map(lambda m: s.send(m), masks)))
+    return filter(lambda km: isinstance(km[1].data, pd.Series) and km[1].data.any(),
+                  zip(keys, map(lambda m: Leaf(s.send(m)), masks)))
 
 
 def root(filters):
@@ -32,12 +39,12 @@ def root(filters):
 
 
 def branch(d, k, filters):
-    d[k] = dict(sieve_stack(d[k], filters))
+    d[k] = dict(sieve_stack(d[k].data, filters))
     return d[k]
 
 
 def extend(d, filters):
-    state = d.pop(None)
+    state = d.pop(None).data
     d.update(sieve_stack(state, filters))
     return d
 
@@ -60,11 +67,11 @@ def dict2tree(d, *parents):
         n = n.add_child(name=k)
         if isinstance(v, dict):
             n.add_child(dict2tree(v, *parents, k))
-        elif isinstance(v, pd.Series):
-            label = str((*parents, k)) if v.any() else '()'
+        elif isinstance(v.data, pd.Series):
+            label = str((*parents, k)) if v.data.any() else '()'
             n.add_child(name=label)
         else:
-            n.add_child(name=str(v))
+            n.add_child(name=str(v.data))
     n.delete()
     return root
 
@@ -72,14 +79,14 @@ def dict2tree(d, *parents):
 class SieveTree:
 
     def __init__(self, filters):
-        self.data = root(filters)
+        self.d = root(filters)
 
     def branch(self, filters, *keys, inplace=False):
         if inplace:
             new_tree = self
         else:
             new_tree = copy.deepcopy(self)
-        d = new_tree.data
+        d = new_tree.d
         for k in iter(keys[:-1]):
             d = d[k]
 
@@ -91,7 +98,7 @@ class SieveTree:
             new_tree = self
         else:
             new_tree = copy.deepcopy(self)
-        d = new_tree.data
+        d = new_tree.d
         for k in iter(keys):
             d = d[k]
 
@@ -99,15 +106,15 @@ class SieveTree:
         return None if inplace else new_tree
 
     def traverse_leaves(self, *keys, from_key=None):
-        d = self.data
+        d = self.d
         for k in iter(keys):
             d = d[k]
 
-        return filter(lambda kv: isinstance(kv[1], pd.Series) and kv[1].any(),
+        return filter(lambda kv: isinstance(kv[1].data, pd.Series) and kv[1].data.any(),
                       recurse_items(d, from_key=from_key))
 
     def get_tree(self):
-        return dict2tree(self.data)
+        return dict2tree(self.d)
 
     def __repr__(self):
         return self.get_tree().get_ascii(show_internal=True)
@@ -137,10 +144,11 @@ if __name__ == '__main__':
     ))
 
     # st4.extend([(None, None), (None, None)])
-    # st4.data[None] &= False
+    # for k, m in st4.traverse_leaves('odd'):
+    #     m.data = 'odd'
     for k, m in st4.traverse_leaves():
         print(k)
-        print(x[m])
+        print(x[m.data])
     print()
 
     print(st4)
